9312b3c485e4a4663524e48c54ca93d9d5c4f327
Fred Siika <fredsiika@gmail.comm>
2020-01-29 21:22:49 -0600

    Added coding resources for section 1

---
+++ Section-1/anatomy-of-a-node-application/index.js
@@ -0,0 +1,25 @@
+/*
+ * Title: Basic Node Example
+ * Description: Simple file that declares a few functions and invokes them.
+ * Author: Fred Siika
+ */
+
+// Dependencies 
+
+
+// App object 
+
+
+// Configuration 
+
+
+// Function that prints a random joke 
+
+
+// Function that loops indefinitely, calling the printAJoke function as it goes // Invoke the loop
+
+
+    // Create the interval, using the config variable defined above
+
+
+// Invoke the loop
---
+++ Section-1/anatomy-of-a-node-application/lib/math.js
@@ -0,0 +1,16 @@
+/*
+ * Title: Math Library
+ * Description: Utility library for math-related functions
+ * Author: Fred Siika
+ */
+
+ 
+// App object 
+
+
+// Get a random integer between two integers 
+// Inspired by: http://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript // Export the library
+
+
+
+// Export the library
