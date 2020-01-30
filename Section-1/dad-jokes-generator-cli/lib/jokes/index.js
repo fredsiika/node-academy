9246703d97132b6ca4faa1e0a692b9488836a28a
Fred Siika <fredsiika@gmail.comm>
2020-01-30 11:43:12 -0600

    made new directory dad-jokes-generator-cli

---
+++ Section-1/dad-jokes-generator-cli/lib/jokes/index.js
@@ -0,0 +1,20 @@
+// Dependencies
+/**
+ * Title: Jokes Library
+ * Description: Utility for getting a list of Jokes
+ * Author: Fred Siika
+ */
+
+// App object
+
+
+// Get all the jokes and return them to the user
+    
+    // Read the text file containing the jokes
+
+    // Turn the string into an array
+    
+    // Return the array
+
+
+// Export the library
