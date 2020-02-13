040b1a60ffa65b5ac5b19fefe0899af8a73c492f
Fred Siika <fredsiika@gmail.comm>
2020-02-13 11:25:23 -0600

    converted to es6

--- Section-1/FINAL/index.js
+++ Section-1/FINAL/index.js
@@ -1,45 +1,30 @@
-/*
- * Title: Basic Node Example
- * Description: Simple file that declares a few functions and invokes them.
- * Author: Fred Siika
-**/
+// Main CLI Entry Point
+// Simple file that declares a few functions and invokes them.
 
-// Dependencies
-var mathLib = require('./lib/math');
-var jokesLib = require('./lib/jokes');
+const mathLib = require('./lib/math');
+const jokesLib = require('./lib/jokes');
 
-// App object
-var app = {};
+const app = {};
 
-// Configuration
 app.config = {
     'timeBetweenJokes' : 1000
 };
 
-// Function that prints a random joke
-app.printAJoke = function(){
+app.printAJoke = () => {
 
-    // Get array of all the jokes
-    var allJokes = jokesLib.allJokes();
+    let allJokes = jokesLib.allJokes();
 
-    // Get the length of the jokes array
-    var numberOfJokes = allJokes.length;
+    let numberOfJokes = allJokes.length;
 
-    // Pick a random number between 1 and the number of jokes
-    var randomJoke = mathLib.getRandomNumber(1, numberOfJokes);
+    let randomJoke = mathLib.getRandomNumber(1, numberOfJokes);
 
-    // Get the joke at that position in the array (minus one)
-    var selectedJoke = allJokes[randomJoke - 1];
+    let selectedJoke = allJokes[randomJoke - 1];
 
-    // Send the joke to the console
     console.log(selectedJoke);
-};
+}
 
-// Function that loops indefinitely, calling the printAJoke function as it goes
-app.indefiniteLoop = function() {
-     // Create the interval, using the config variable defined above
+app.indefiniteLoop = () => {
      setInterval(app.printAJoke, app.config.timeBetweenJokes);
-};
-   
-// Invoke the loop
+}
+
 app.indefiniteLoop();
--- Section-1/FINAL/lib/jokes/index.js
+++ Section-1/FINAL/lib/jokes/index.js
@@ -1,28 +1,17 @@
 /**
  * Title: Jokes Library
  * Description: Utility for getting a list of Jokes
- * Author: Fred Siika
 */
 
-// Dependencies
 const fs = require('fs');
 
-// App object
-var jokes = {};
+const jokes = {};
 
-// Get all the jokes and return them to the user
-jokes.allJokes = function() {
-    
-    // Read the text file containing the jokes
+jokes.allJokes = () => {
     var fileContents = fs.readFileSync(__dirname+'/jokes.txt', 'utf8');
-
-    // Turn the string into an array
     var arrayOfJokes = fileContents.split(/\r?\n/);
-
-    // Return the array
     return arrayOfJokes;
-
-};
+}
     
 // Export the library
 module.exports = jokes;
